582af403a89567656ea49c7d68ff7ad0b55c0ad6
APersonWithNoName <wyb20102024@outlook.com>
2024-08-09 19:50:33 +0800

    基本完成prompt部分

--- src/prompt.c
+++ src/prompt.c
@@ -4,30 +4,53 @@
 #include <unistd.h>
 #include <string.h>
 
+#include "./shell.h"
+
 #define MAX_HOST_NAME_LEN 256
 #define MAX_USER_NAME_LEN 256
-#define MAX_PATH_NAME_LEN 1024
+#define MAX_DIR_NAME_LEN 1024
 #define MAX_PORMPT_LEN 2048
 
 char* prompt() {
-    static char FORMPT[MAX_PORMPT_LEN];
-    struct passwd *PWD;
+    static char PROMPT[MAX_PORMPT_LEN];
+    struct passwd *PWD = getpwuid(getuid());
     char HOST_NAME[MAX_HOST_NAME_LEN];
     char USER_NAME[MAX_USER_NAME_LEN];
-    char PATH_NAME[MAX_PATH_NAME_LEN];
-    PWD = getpwuid(getuid());
-    getcwd(PATH_NAME, MAX_PATH_NAME_LEN);
-    if(gethostname(HOST_NAME, MAX_HOST_NAME_LEN) == 0) {
-        sprintf(FORMPT, "[Small-Shell] %s@%s:%s", PWD -> pw_name, HOST_NAME, PATH_NAME);
+    char DIR_NAME[MAX_DIR_NAME_LEN];
+    char user_str;
+    char prompt_end[MAX_DIR_NAME_LEN];
+    getcwd(DIR_NAME, MAX_DIR_NAME_LEN);
+    
+    if(PWD == NULL || \
+       gethostname(HOST_NAME,MAX_HOST_NAME_LEN)==-1 || \
+       getcwd(DIR_NAME,256)==NULL) {
+        strcpy(PROMPT, "Small-Shell>> ");
+        fflush(stdout);
+        return PROMPT;
     } else {
-        sprintf(FORMPT, "[Small-Shell] %s@Unknown:%s", PWD -> pw_name, PATH_NAME);
+        if(getuid() == 0) {
+            user_str = '#';
+        } else {
+            user_str = '$';
+        }
+        /*if(gethostname(HOST_NAME, MAX_HOST_NAME_LEN) == 0) {
+            sprintf(PROMPT, "[Small-Shell] %s@%s:%s%c", PWD -> pw_name, HOST_NAME, DIR_NAME, user_str);
+            return PROMPT;
+        } else {
+            sprintf(PROMPT, "[Small-Shell] %s@Unknown:%s%c", PWD -> pw_name, DIR_NAME, user_str);
+            return PROMPT;
+        }*/
+        sprintf(PROMPT, "[Small-Shell] %s@%s:%s%c", PWD -> pw_name, HOST_NAME, DIR_NAME, user_str);
+        return PROMPT;
+        /*if(strncmp(PROMPT, PWD -> pw_dir, strlen(PWD -> pw_dir)) != 0) {
+            sprintf(prompt_end, "%s%c", DIR_NAME, user_str);
+            strcat(PROMPT, prompt_end);
+            return PROMPT;
+        } else {
+            sprintf(prompt_end, "~/%s%c", DIR_NAME, user_str);
+        }*/
     }
-    /*if(getuid() == 0) {
-        sprintf(FORMPT, "#");
-    } else {
-        sprintf(FORMPT, "#");
-    }*/
-    return FORMPT;
+    
 }
 
 int main() {
